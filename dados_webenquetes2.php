ab74b17db9f7090a5a0b3c8506b8c172b3755abc
tuliorubim <77076471+tuliorubim@users.noreply.github.com>
2023-05-11 20:26:07 -0300

    Add files via upload

--- dados_webenquetes2.php
+++ dados_webenquetes2.php
@@ -4,7 +4,7 @@ trait Dados_webenquetes2 {
 	public static function setFormTabela6($idu) {
 		$variaveis1 = array("idCliente", "nome", "empresa", "site", "logo", "logoReduzida", "data_cadastro", "usuario", "senha", "repsenha");
 		$tipos1 = array("integer", "varchar", "varchar", "varchar", "blob", "blob", "datetime", "varchar", "varchar", "varchar");
-		$labels1 = array("", "Nome ", "Empresa ", "Seu site ", "An&uacute;ncio em imagem ", '', '', "E-mail $star ", "Senha $star ", "Repetir Senha $star ", true);
+		$labels1 = array("", "Nome ", "Empresa ", "Seu site ", "An&uacute;ncio em imagem ", '', '', "E-mail ", "Senha ", "Repetir Senha ", true);
 		$inputs1 = array("hidden", "text", "text", "text", "file", "hidden", "hidden", "text", "password", "password");
 		$enderecos1 = array("logos", "logosReduzidas");
 		
