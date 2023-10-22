f15bcd5867f048916f3ababdd1d2fb2e8c154a8c
tuliorubim <77076471+tuliorubim@users.noreply.github.com>
2023-10-21 22:15:29 -0300

    Add files via upload

--- sign_up_from_fb.php
+++ sign_up_from_fb.php
@@ -1,22 +1,23 @@
 <?php
 include "ajax_header.php";
-$status = $_POST['status'];
 $idCliente = $_POST['idCliente'];
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $db->select("select idCliente, usuario from cliente where usuario = '$email'", array("idc", "usuario"));
 $json = '';
-if ($status === 'connected') {
-	if (empty($usuario)) {
+if (empty($usuario)) {
+	$db->select("select usuario from cliente where idCliente = $idCliente", array("email2"));
+	if (empty($email2)) {
 		mysqli_query($con, "update cliente set nome = '$nome', usuario = '$email' where idCliente = $idCliente");
-		$json = '{ "status" : "Signed up" }';
-		$_SESSION['user'] = $idCliente;
-	} elseif ($idCliente != $idc) {
-		$_SESSION['user'] = $idc;
-		$json = '{ "status" : "Logged in" }';
+	} else {
+		mysqli_query($con, "insert into cliente (nome, usuario) values ('$nome', '$email')");
+		$db->select("select idCliente from cliente where usuario = '$email'", array("idCliente2"));
+		$_SESSION['user'] = $idCliente2;
 	}
-) else {
-	$_SESSION['user'] = NULL;
+	$json = '{ "status" : "Signed up" }';
+} elseif ($idCliente != $idc) {
+	$_SESSION['user'] = $idc;
+	$json = '{ "status" : "Logged in" }';
 }
 echo $json;
 ?>
